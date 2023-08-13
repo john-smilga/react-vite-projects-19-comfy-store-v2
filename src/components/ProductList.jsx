9529b3ff265aad821a04ad487a060806a0b51829
john smilga <johnsmilga@gmail.com>
2023-08-13 15:21:13 -0400

    fix products list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -13,14 +13,14 @@ const ProductList = () => {
           <Link
             key={product.id}
             to={`/products/${product.id}`}
-            className='p-8 rounded-lg flex  flex-wrap bg-base-100 shadow-xl hover:shadow-2xl duration-300 group'
+            className='p-8 rounded-lg flex flex-col sm:flex-row gap-y-4 flex-wrap bg-base-100 shadow-xl hover:shadow-2xl duration-300 group'
           >
             <img
               src={image}
               alt={title}
               className='h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover group-hover:scale-105 transition duration-300'
             />
-            <div className='ml-8 sm:ml-16'>
+            <div className='ml-0 sm:ml-16'>
               <h3 className='capitalize font-medium text-lg'>{title}</h3>
               <h4 className='capitalize text-md text-neutral-content'>
                 {company}
@@ -29,7 +29,9 @@ const ProductList = () => {
               {/* COLOR */}
             </div>
 
-            <p className='font-medium ml-auto text-lg'>{formatPrice(price)}</p>
+            <p className='font-medium ml-0 sm:ml-auto text-lg'>
+              {formatPrice(price)}
+            </p>
           </Link>
         );
 
